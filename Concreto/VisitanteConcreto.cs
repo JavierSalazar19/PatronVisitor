730e836ccd8f3c38f386819355c80dddf9633f7c
JavierSalazar19 <javierasc2002@gmail.com>
2023-12-06 08:17:41 -0700

    Agregar archivos de proyecto.

---
+++ Concreto/VisitanteConcreto.cs
@@ -0,0 +1,44 @@
+﻿using PatronVisitor.Components;
+using PatronVisitor.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatronVisitor.Concreto
+{
+    public class VisitanteConcreto : IVisitor
+    {
+        public void Visitar(Procesador componente)
+        {
+            Console.WriteLine(string.Format("Procesador s/n {0}", componente.Serial));
+        }
+        public void Visitar(DiscoRigido componente)
+        {
+            Console.WriteLine(string.Format("DiscoRigido s/n {0}", componente.Serial));
+        }
+        public void Visitar(PlacaBase componente)
+        {
+            Console.WriteLine(string.Format("PlacaBase s/n {0}", componente.Serial));
+        }
+
+    }
+    public class VisitanteConcreto2 : IVisitor
+    {
+        public void Visitar(Procesador componente)
+        {
+            Console.WriteLine(String.Format("Procesador 2 s/n {0}", componente.Serial));
+        }
+
+        public void Visitar(PlacaBase componente)
+        {
+            Console.WriteLine(String.Format("Placa Base 2 s/n {0}", componente.Serial));
+        }
+
+        public void Visitar(DiscoRigido componente)
+        {
+            Console.WriteLine(String.Format("Disco Rigido 2 s/n {0}", componente.Serial));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+using PatronVisitor.Component;
+using PatronVisitor.Components;
+using PatronVisitor.Concreto;
+using PatronVisitor.Interface;
+
+IVisitor visitante1 = new VisitanteConcreto();
+
+Componente _Dr1 = new DiscoRigido("Disa231231312");
+Componente _Pb1 = new PlacaBase("Placaa123131");
+Componente _P1 = new Procesador("Procesador123123");
+
+_Dr1.Aceptar(visitante1);
+_Pb1.Aceptar(visitante1);
+_P1.Aceptar(visitante1);
+
+IVisitor visitante2 = new VisitanteConcreto2();
+
+Componente dr2 = new DiscoRigido("1231DSFFSD3-DR");
+Componente pb2 = new PlacaBase("dkk3nndj12313-PB");
+Componente p2 = new Procesador("9393SKKK4K3-P");
+
+
+dr2.Aceptar(visitante2);
+pb2.Aceptar(visitante2);
+p2.Aceptar(visitante2);
+Console.ReadKey();
